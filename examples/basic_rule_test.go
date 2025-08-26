31e1e2e1443b72f92be7932c01e9594629c88c56
hungpdn <phamdongochung@gmail.com>
2025-08-27 02:29:58 +0700

    examples: add basic rule

---
+++ examples/basic_rule_test.go
@@ -0,0 +1,41 @@
+package examples
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hungpdn/grule-plus/engine"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasicRule(t *testing.T) {
+	cfg := engine.Config{
+		Type:            0, // LRU
+		Size:            1000,
+		CleanupInterval: 10,
+		TTL:             60,
+		Partition:       4,
+	}
+	grule := engine.NewPartitionEngine(cfg, nil)
+
+	rule := "DiscountRule"
+	statement := `rule DiscountRule "Apply discount" salience 10 {
+	 			when 
+					DiscountFact.Amount > 100 
+				then 
+					DiscountFact.Discount = 10; 
+					Retract("DiscountRule");
+				}`
+
+	grule.AddRule(rule, statement, 60)
+
+	fact := struct {
+		Amount   int
+		Discount int
+	}{Amount: 150}
+
+	err := grule.Execute(context.Background(), rule, &fact)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 10, fact.Discount)
+}
